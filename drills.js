16e800d1ec6cb502cbf92fb94d785ddc8a9ce146
Filipp Gorbunov <Filippgorb@gmail.com>
2018-08-28 09:16:24 -0700

    completed drills

---
+++ drills.js
@@ -0,0 +1,87 @@
+//Even or odd
+//O(1)
+
+//Are you here?
+//O(n^2)
+
+//Doubler
+//O(n)
+
+//Naive Search
+//O(n)
+
+//Creating pairs:
+//O(n^2)
+
+//Computing fibonaccis
+//O(n)
+
+//An Efficient Search
+//O(log(n))
+
+//Random element
+//O(1)
+
+//Is it prime?
+//O(n)
+
+
+//Recursive Big O
+//Exercise 1 - Counting Sheep
+//O(n)
+
+//Exercise 2: Array Double
+//O(n)
+
+//Exercise 3 - Reverse String
+//O(n)
+
+//Exercise 4 - Triangular Number
+//O(n^2)
+
+//Exercise 5 - String Splitter
+//O(n)
+
+//Exercise 6 - Binary Representation
+//O(log(n))
+
+//Exercise 7 - Factorial
+//O(n^2)
+
+//Exercise 8 - Fibonacci
+//O(2^n)
+
+//Exercise 9 - Anagrams
+//O(2^n)
+
+//Exercise 10: Animal Hierarchy
+//O(n^2)
+
+//Exercise 11: Organization Chart
+//O(n^2)
+
+
+//Iterative Big O
+//Exercise 1 - Counting Sheep
+//O(n)
+
+//Exercise 2: Array Double
+//O(n)
+
+//Exercise 3 - Reverse String
+//O(n)
+
+//Exercise 4 - Triangular Number
+//O(n)
+
+//Exercise 5 - String Splitter
+//O(n)
+
+//Exercise 6 - Binary Representation
+//O(log(n))
+
+//Exercise 7 - Factorial
+//O(n)
+
+//Exercise 8 - Fibonacci
+//O(n)
